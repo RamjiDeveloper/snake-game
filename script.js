7add26a5db6d053374d93203597e2fba5970056e
RamjiDeveloper <ramji54902@gmail.com>
2026-04-03 01:07:40 +0530

    Compelte code uploded

--- script.js
+++ script.js
@@ -1,12 +1,38 @@
 const board = document.querySelector(".board");
 
+const startButton = document.querySelector('.start-button')
+const modal = document.querySelector('.modal')
+
+const startGameModal = document.querySelector('.start-game');
+const gameOverModal = document.querySelector('.game-over');
+const restartButton = document.querySelector('.btn-restart');
+
+// Score & Timer Elements
+const highScoreElement = document.querySelector('#high-score');
+const scoreElement = document.querySelector('#score');
+const timeElement = document.querySelector('#time');
+
+
 const blockHeight = 30;
 const blockWidth = 30;
 
+
+let hightScore = localStorage.getItem('highScore') || 0;
+let score = 0;
+let time = `00:00:00`;
+
+ highScoreElement.innerText = hightScore;
+
+// Grid setup
 const cols = Math.floor(board.clientWidth / blockWidth);
 const rows = Math.floor(board.clientHeight / blockHeight);
 
 let intervalId = null;
+
+
+let timerIntervalId = null;
+
+
 
 let food = {
   x: Math.floor(Math.random() * rows),
@@ -14,7 +40,7 @@ let food = {
 };
 
 const blocks = [];
-const snake = [
+let snake = [
   {
     x: 1,
     y: 3,
@@ -29,6 +55,7 @@ let direction = "down";
 //     board.appendChild(block)
 // }
 
+// Create board
 for (let row = 0; row < rows; row++) {
   for (let col = 0; col < cols; col++) {
     const block = document.createElement("div");
@@ -39,6 +66,8 @@ for (let row = 0; row < rows; row++) {
   }
 }
 
+
+// Render game
 function render() {
   let head = null;
   blocks[`${food.x}-${food.y}`].classList.add("food");
@@ -55,14 +84,28 @@ function render() {
 
 //   Self collision
 if (snake.slice(0, -1).some(segment => segment.x === head.x && segment.y === head.y)) {
-    alert("Game Over");
     clearInterval(intervalId);
-  }
 
+    modal.style.display = 'flex';
+    startGameModal.style.display = 'none';
+    gameOverModal.style.display = 'block'; 
+
+    return;
+}
+
+  // Wall collision
   if (head.x < 0 || head.x >= rows || head.y < 0 || head.y >= cols) {
-    alert("Game Over");
+    // alert("Game Over");
     clearInterval(intervalId);
+
+    // show game over modal
+    modal.style.display = 'flex';
+    startGameModal.style.display = 'none';
+    gameOverModal.style.display = 'block'; 
+
+    return;
   }
+  // food consume logic
   if (head.x === food.x && head.y === food.y) {
     blocks[`${food.x}-${food.y}`].classList.remove("food");
     food = {
@@ -72,6 +115,16 @@ if (snake.slice(0, -1).some(segment => segment.x === head.x && segment.y === hea
     // foodconsume with the help of snake unshipt head and add food to snake
     blocks[`${food.x}-${food.y}`].classList.add("food");
     snake.unshift(head)
+
+    score += 10;
+    scoreElement.innerText = score;
+
+    if(score > hightScore) {
+        hightScore = score;
+        localStorage.setItem('highScore', hightScore.toString());
+        
+       
+    }
   }
 
   snake.forEach((segment) => {
@@ -92,14 +145,80 @@ if (snake.slice(0, -1).some(segment => segment.x === head.x && segment.y === hea
   });
 }
 
-intervalId = setInterval(() => {
-  render();
-}, 500);
+// intervalId = setInterval(() => {
+//   render();
+// }, 400);
 
-// ArrowUp
-// script.js:77 ArrowRight
-// script.js:77 ArrowDown
-// script.js:77 ArrowLeft
+
+
+// satrt game on click of start button and stop the modal
+startButton.addEventListener('click', () => {
+  modal.style.display = 'none';
+  intervalId = setInterval(() => {render()}, 400);
+
+  // timerIntervalId
+  timerIntervalId = setInterval(() => {
+    let [hours, minutes, seconds] = time.split(':').map(Number);
+   if(seconds == 59) {
+    minutes += 1;
+    seconds = 0;
+   } else if(minutes == 59) {
+    hours += 1;
+    minutes = 0;
+   } else {
+    seconds += 1;
+   }
+    time = `${hours}:${minutes}:${seconds}`;
+    timeElement.innerText = time;
+  }, 1000);
+
+});
+
+
+restartButton.addEventListener('click', restartGame);
+
+
+
+//restart game on click of restart button and stop the modal function and reset the game
+
+function restartGame() {
+  // reset the game state
+
+  blocks[`${food.x}-${food.y}`].classList.remove("food");
+  snake.forEach((segment) => {
+    const block = blocks[`${segment.x}-${segment.y}`];
+    if (block) {
+      block.classList.remove("fill");
+    }
+  });
+
+
+  // score reset
+  score = 0;
+  time = `00:00:00`;
+  
+  scoreElement.innerText = score;
+  // timeElement.innerText = time;
+  highScoreElement.innerText = hightScore;
+
+
+  modal.style.display = 'none';
+  direction = "down";
+  snake = [
+    {
+      x: 1, y: 3,
+    },
+  ];
+   food = {
+    x: Math.floor(Math.random() * rows),
+    y: Math.floor(Math.random() * cols),
+  };
+  intervalId = setInterval(() => {render()}, 400);
+  
+}
+
+
+// Controls
 addEventListener("keydown", (e) => {
   if (e.key === "ArrowUp" && direction !== "down") {
     direction = "up";
